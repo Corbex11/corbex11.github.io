d14b093353ad338f0504efffa2521f2235b4b72a
Callum Fisher <callumfisher2006@gmail.com>
2021-11-20 22:34:58 +0000

    Changed Theming
    - Updated stylesheet with darker colours
    - Updated icons

--- scripts/mcTerminator.js
+++ scripts/mcTerminator.js
@@ -1,14 +1,13 @@
 /* callum fisher - corbex11@gmail.com
-last updated: 4/11/2021 */
+last updated: 20/11/2021 */
 
 window.onload = function() {
-    var homessi = 0
+    var count = 0;
     setInterval(function() {
-        console.log("test")
-    	homessi++
-	    document.getElementById("slideshow").src = `media/homess${homessi}.png`
-	    if (homessi == 2) {
-	    	homessi = 0
+    	count++
+	    document.getElementById("slideshow").src = `media/mcTerminator${ssi}.png`;
+	    if (count == 2) {
+	    	count = 0;
 	    }
-    }, 4000)
+    }, 4000);
 }
